ddfd53b76d4b8b7cac18dda87bf74658ed7814ff
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-08-10 18:47:28 +0900

    Feat: @Pattern에서의 exception 처리

--- src/main/java/com/pay1oad/homepage/exception/GlobalExceptionHandler.java
+++ src/main/java/com/pay1oad/homepage/exception/GlobalExceptionHandler.java
@@ -5,8 +5,13 @@ import com.pay1oad.homepage.response.ResForm;
 import com.pay1oad.homepage.response.code.status.ErrorStatus;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -20,6 +25,17 @@ public class GlobalExceptionHandler {
                 null
         );
         return new ResponseEntity<>(response, errorStatus.getHttpStatus());
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<?> handleValidationExceptions(MethodArgumentNotValidException ex) {
+        Map<String, String> errors = new HashMap<>();
+        ex.getBindingResult().getAllErrors().forEach((error) -> {
+            String fieldName = ((FieldError) error).getField();
+            String errorMessage = error.getDefaultMessage();
+            errors.put(fieldName, errorMessage);
+        });
+        return new ResponseEntity<>(errors, HttpStatus.BAD_REQUEST);
     }
 
 }
