28c7527ef325d258452b0926a83f14d2081c12c5
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-08-24 21:59:00 +0900

    Feat: Swagger API 설명 추가 및 gradle 수정

--- src/main/java/com/pay1oad/homepage/controller/login/EmailVerificationController.java
+++ src/main/java/com/pay1oad/homepage/controller/login/EmailVerificationController.java
@@ -12,6 +12,7 @@ import com.pay1oad.homepage.security.JwtUtils;
 import com.pay1oad.homepage.service.email.EmailService;
 import com.pay1oad.homepage.service.login.EmailVerificationService;
 import com.pay1oad.homepage.service.login.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -37,6 +38,7 @@ public class EmailVerificationController {
     private final JwtUtils jwtUtils;
     private final MemberRepository memberRepository;
 
+    @Operation(summary = "회원가입 시 인증 이메일을 발송하는 API", description = "jwt토큰을 조회하여 해당 jwt 주인의 이메일로 인증 이메일이 발신됩니다.")
     @GetMapping("/verify/sendmail")
     public ResponseEntity<?> sendVerificationEmail(@RequestHeader("Authorization") String authorizationHeader) {
         try {
@@ -54,6 +56,7 @@ public class EmailVerificationController {
         }
     }
 
+    @Operation(summary = "회원가입 시 이메일을 인증하는 API", description = "발송된 이메일에서 주어진 링크를 클릭하면 해당 api가 실행됩니다.")
     @GetMapping("/verify/email")
     public String verifyEmail(@RequestParam String id) {
         try {
--- src/main/java/com/pay1oad/homepage/controller/login/MemberController.java
+++ src/main/java/com/pay1oad/homepage/controller/login/MemberController.java
@@ -10,6 +10,7 @@ import com.pay1oad.homepage.response.code.status.ErrorStatus;
 import com.pay1oad.homepage.security.TokenProvider;
 import com.pay1oad.homepage.service.login.JwtRedisService;
 import com.pay1oad.homepage.service.login.MemberService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -52,6 +53,7 @@ public class MemberController {
         this.verificationService = verificationService;
     }*/
 
+    @Operation(summary = "회원가입 API", description = "username, email, passwd를 입력하세요. 나머지 DTO 형식은 무시해도 됩니다.")
     @PostMapping("/signup")
     public ResponseEntity<?> registerUser(@RequestBody MemberDTO memberDTO) {
         try {
@@ -86,6 +88,7 @@ public class MemberController {
         }
     }
 
+    @Operation(summary = "로그인 API", description = "username, passwd를 입력하세요. 나머지 DTO 형식은 무시해도 됩니다.")
     @PostMapping("/signin")
     public ResponseEntity<?> authenticate(@Valid @RequestBody LoginRequestDTO.toSignInDTO signInDTO){
         Member member=memberService.getByCredentials(
@@ -120,6 +123,7 @@ public class MemberController {
 
     }
 
+    @Operation(summary = "로그아웃 API", description = "JWT 토큰으로 해당 유저를 로그아웃 합니다. 서버에서 기존 토큰을 무효화 합니다.")
     @PostMapping("/signout")
     public String signout(){
 
@@ -159,6 +163,7 @@ public class MemberController {
 
     }
 
+    @Operation(summary = "토큰 유효화 API", description = "입력한 JWT 토큰을 유효화 합니다. 유효화 기간은 10분 입니다.")
     @PostMapping("/refreshToken")
     public String refresh(@RequestHeader("Authorization") String authorizationHeader){
         //get token
