8137a7862d8f9510a314b6aadaba77764ff7c9ce
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-09-07 21:46:12 +0900

    Feat: Enum 저장 형식 수정

--- src/main/java/com/pay1oad/homepage/model/login/Member.java
+++ src/main/java/com/pay1oad/homepage/model/login/Member.java
@@ -4,20 +4,13 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
+import jakarta.persistence.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import com.pay1oad.homepage.model.board.Board;
 import com.pay1oad.homepage.model.board.Comment;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -50,6 +43,7 @@ public class Member implements UserDetails {
     private String email;
 
 	@Column(nullable = false)
+	@Enumerated(value = EnumType.STRING)
 	private MemberAuth memberAuth;
 
     private String authProvider;
